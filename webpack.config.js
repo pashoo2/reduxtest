f84706e61ff6d55f2f7a0de2f30b0eafa4779089
Akulich Paul <pashoo2@users.noreply.github.com>
2017-10-02 12:29:23 +0400

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+    devtool: 'eval-source-map',
+    resolve : {
+        alias : {
+            "components"  : path.resolve(__dirname, "./src/components"),
+            "actions"     : path.resolve(__dirname, "./src/actions"),
+            "sagas"       : path.resolve(__dirname, "./src/sagas"),
+            "middlewares" : path.resolve(__dirname, "./src/middlewares")
+        }    
+    },
+    entry : [
+        'babel-polyfill',
+        './src/index.js',
+    ],
+    output : {
+        path : path.join(__dirname, 'dist'),
+        filename : 'bundle.js',
+        publicPath : '/static'
+    },
+    node : {
+        fs: "empty",
+        path : true
+    },
+    module: {
+        rules: [
+            {   test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            { 
+                test: /\.jsx$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(css|scss)$/, 
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader'
+            }
+        ]
+        
+    },
+    plugins : [new webpack.HotModuleReplacementPlugin()],
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      filename : 'bundle.js',
+      compress: true,
+      hot : true,
+      inline : true,
+      lazy : false,
+      clientLogLevel : 'error',
+      public : 'redux-test-pashoo.c9users.io',
+      port: process.env.PORT,
+      host: process.env.IP,
+      publicPath : '/static'
+    }
+};
